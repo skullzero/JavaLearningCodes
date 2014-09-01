d288955b5b3f0727e6c0ad435db6500dacfd2a64
skullzero <skullzero@163.com>
2014-09-01 23:54:40 +0800

    add codes for Thread testing

--- src/main/java/skullzero/java/basic/thread/CreateTickets.java
+++
@@ -1,6 +0,0 @@
-package skullzero.java.basic.thread;
-
-public class CreateTickets extends Thread
-{
-
-}
--- src/main/java/skullzero/java/basic/thread/TestThread.java
+++ src/main/java/skullzero/java/basic/thread/TestThread.java
@@ -1,5 +1,19 @@
 package skullzero.java.basic.thread;
 
-public class TestThread {
+import org.junit.*;
+
+public class TestThread 
+{
+	private String threadName;
+	
+	@Test
+	public void testingStart()
+	{
+		for(int i = 1; i < 5; i++)
+		{
+			new Thread(new OperateFile("Thread" + i)).start();
+		}
+	}
+	
 
 }
