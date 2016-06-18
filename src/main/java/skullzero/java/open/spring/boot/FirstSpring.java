528f9d004579a9e598a95989c71b9971f665f311
skullzero <skullzero@163.com>
2016-06-18 23:22:42 +0800

    Change the package of spring learning codes

--- src/main/java/skullzero/java/open/spring/boot/FirstSpring.java
+++ src/main/java/skullzero/java/open/spring/boot/FirstSpring.java
@@ -1,4 +1,4 @@
-package skullzero.java.spring.boot;
+package skullzero.java.open.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
