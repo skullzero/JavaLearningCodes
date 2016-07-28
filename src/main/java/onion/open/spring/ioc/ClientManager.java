8c7c228ee33241b43db6d3715d81ccfe1aa60adb
skullzero <skullzero@163.com>
2016-07-29 00:09:54 +0800

    Add some comments

--- src/main/java/onion/open/spring/ioc/ClientManager.java
+++ src/main/java/onion/open/spring/ioc/ClientManager.java
@@ -5,11 +5,12 @@ public class ClientManager
 	private Client client;
 	private Client client2;
 	
+	//构造函数注入
 	public ClientManager(Client client)
 	{
 		this.client = client;
 	}
-	
+	//属性注入
 	public void setClient2(Client client2)
 	{
 		this.client2 = client2;
--- src/main/java/onion/open/spring/ioc/TestIoC.java
+++ src/main/java/onion/open/spring/ioc/TestIoC.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.beans.factory.BeanFactory;
 
+/**
+ * IoC容器，最主要的就是完成对象的创建以及维护对象的依赖关系等。
+ * 控制被反转之后，获得依赖对象的过程被反转了，获得依赖对象的过程由自身管理变为了由IOC容器主动注入。
+ * 更合适的名字-DI Dependency Injection 依赖注入
+ * http://blog.csdn.net/m13666368773/article/details/7802126 解释较清晰
+ */
+
+
 public class TestIoC 
 {
 	@Test
