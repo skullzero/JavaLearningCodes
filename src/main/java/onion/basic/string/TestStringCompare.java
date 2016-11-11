f48d81161dec2e3de700a44d746e977730dc7660
skullzero <skullzero@163.com>
2016-11-11 14:31:58 +0800

    新增两个新方法用于验证值传递

--- src/main/java/onion/basic/string/TestStringCompare.java
+++ src/main/java/onion/basic/string/TestStringCompare.java
@@ -1,9 +1,14 @@
 package onion.basic.string;
 
+import javassist.compiler.ast.StringL;
+
 import org.junit.*;
 
 public class TestStringCompare 
 {
+	private String stringK = "aaa1";
+	private String stringM = new String("aaa2");
+	
 	@Test
 	public void testingStart()
 	{
@@ -17,7 +22,7 @@ public class TestStringCompare
 		String stringB = new String("aaa");
 		System.out.println("if stringA == stringB? " + (stringA == stringB));
 		
-		//只有"包围的字符串会放到字符串池中
+		//只有双引号包围的字符串会放到字符串池中，编译时就被当做了常量
 		String stringC = "bbb";
 		String stringD = "bbb";
 		System.out.println("if stringC == stringD? " + (stringC == stringD));
@@ -27,6 +32,7 @@ public class TestStringCompare
 		System.out.println("if stringE == stringF? " + (stringE == stringF));		
 		
 		String stringG = "ddd";
+		//运行时通过计算得到的是新创建的，所以和常量池中的不一样
 		String stringH = stringG + stringC;
 		String stringI = "dddbbb";
 		System.out.println("if stringH == stringI? " + (stringH == stringI));
@@ -39,5 +45,26 @@ public class TestStringCompare
 		System.out.println("if stringI == stringJ? " + (stringI.equals(stringJ.toString())));
 		//System.out.println(stringA.hashCode());
 		//System.out.println(stringB.hashCode());
+		
+		changeString(stringK);
+		changeString2(stringM);
 	}
+	
+	private void changeString(String stringL)
+	{
+		System.out.println("if stringK == stringL? " + (stringK == stringL));
+		stringL = "abcdefghijklmn";
+		//no change
+		System.out.println("stringK was changed by stringL? stringK=" + stringK);
+	}
+	
+	private void changeString2(String stringN)
+	{
+		System.out.println("if stringM == stringN? " + (stringM == stringN));
+		stringN = new String("aaa2");
+		//指向了不同的对象
+		System.out.println("if stringM == stringN? " + (stringM == stringN));
+		System.out.println("if stringM equals stringN? " + (stringM.equals(stringN)));
+	}	
+	
 }
