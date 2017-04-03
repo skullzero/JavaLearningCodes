578c913bbcf5e4912fbb3b789646f264080de786
skullzero <skullzero@163.com>
2017-04-03 12:07:41 +0800

    添加学习castor的相关代码

---
+++ src/main/java/onion/open/castor/CustomerVO.java
@@ -0,0 +1,29 @@
+package onion.open.castor;
+
+public class CustomerVO 
+{
+	private String customerID;
+	private String customerName;
+	private String birthDate;
+	
+	public String getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+}
---
+++ src/main/java/onion/open/castor/TestCastor.java
@@ -0,0 +1,57 @@
+package onion.open.castor;
+
+import org.exolab.castor.mapping.Mapping;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.Marshaller;
+import java.io.OutputStreamWriter;
+import org.junit.*;
+import java.io.FileReader;
+import org.xml.sax.InputSource;
+
+/*
+ * Castor 提供Java对象和XML自动绑定功能。
+ */
+
+public class TestCastor 
+{
+	private Mapping marshallMapping = new Mapping();
+	private Mapping unmarshallMapping = new Mapping();
+	private CustomerVO customerVO = new CustomerVO();
+	
+	@Test
+	public void testMarshalXML()
+	{
+		try
+		{
+			//1.从mapping XML文件中加载映射信息
+			//maven将main/resources下的文件编译到classes根目录下
+			unmarshallMapping.loadMapping(TestCastor.class.getClassLoader()
+										  .getResource("castor/CustomerVO-Unmarshall-mapping.xml")
+										  .getFile());
+			
+			//2.解编CustomerData.xml，构造Java对象
+			Unmarshaller unmar = new Unmarshaller(unmarshallMapping);
+			customerVO = (CustomerVO)unmar.unmarshal(new InputSource(new FileReader("/Users/onion/git/JavaLearningCodes/target/classes/castor/CustomerData.xml")));
+			System.out.println(customerVO.getCustomerID() + " - " + 
+								customerVO.getCustomerName() + " - " +
+								customerVO.getBirthDate());
+			
+			//3.改造对象数据
+			customerVO.setCustomerID("112233");
+			customerVO.setCustomerName("HAHAHA");
+			customerVO.setBirthDate("1980-01-01");			
+			
+			//4.将Java Object编组为XML并输出到控制台
+			marshallMapping.loadMapping(TestCastor.class.getClassLoader()
+					  .getResource("castor/CustomerVO-Marshall-mapping.xml")
+					  .getFile());			
+			Marshaller marshaller = new Marshaller(new OutputStreamWriter(System.out));
+			marshaller.setMapping(marshallMapping);
+			marshaller.marshal(customerVO);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
