f5e1de0f8a9e3af1a6620233837e4e96e1a72b46
skullzero <skullzero@163.com>
2017-04-15 12:32:12 +0800

    添加logback的readme

---
+++ src/main/java/onion/open/log/TestLogback.java
@@ -0,0 +1,16 @@
+package onion.open.log;
+
+/*
+ * logback-core：其它两个模块的基础模块
+ * logback-classic：它是log4j的一个改良版本，同时它完整实现了slf4j API使你可以很方便地更换成其它日志系统如log4j或Java Logging
+ * logback-access：访问模块与Servlet容器集成提供通过Http来访问日志的功能
+ * 
+ * 
+ * 
+ * 级别排序为： TRACE < DEBUG < INFO < WARN < ERROR
+ */
+
+public class TestLogback 
+{
+
+}
